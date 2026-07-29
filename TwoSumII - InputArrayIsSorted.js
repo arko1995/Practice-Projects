1af53155ba3e8a07a3e57b50e037fcff94a2c806
arko1995 <shahriarsohelarko@gmail.com>
2026-07-29 15:35:44 +0600

    top interview questions

---
+++ TwoSumII - InputArrayIsSorted.js
@@ -0,0 +1,20 @@
+const twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+
+    if (sum === target) {
+      return [left + 1, right + 1];
+    }
+    if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return [];
+};
+twoSum([2, 7, 11, 15], 9);
