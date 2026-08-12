4eb78b315e7ebcf5a2f02ecff2f7cfd195ed91fe
arko1995 <shahriarsohelarko@gmail.com>
2026-08-12 18:53:15 +0600

    dp solution

---
+++ MinimumInsertionStepstoMakeaStringPalindrome.js
@@ -0,0 +1,18 @@
+const minInsertions = function (s) {
+  const n = s.length;
+  const dp = Array.from({ length: n }, () => new Array(n).fill(0));
+
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = i + 1; j < n; j++) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1];
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[0][n - 1];
+};
+
+minInsertions("leetcode");
