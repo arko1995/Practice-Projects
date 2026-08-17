ec9120064d58d6acf15612203635e832aeeb98a6
arko1995 <shahriarsohelarko@gmail.com>
2026-08-17 12:58:36 +0600

    binary search solution

---
+++ PeakIndexinaMountainArray.js
@@ -0,0 +1,16 @@
+const peakIndexInMountainArray = function (arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] < arr[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+};
+peakIndexInMountainArray([0, 10, 5, 2]);
