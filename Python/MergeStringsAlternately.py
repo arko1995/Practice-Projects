defe0d3bf2edcff11058632e863531896184b8ec
arko1995 <shahriarsohelarko@gmail.com>
2026-08-21 18:58:56 +0600

    leetcode 75 on py

---
+++ Python/MergeStringsAlternately.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+
+        maxLength = max(len(word1), len(word2))
+        result = ""
+
+        for i in range(0, maxLength):
+
+            if i < len(word1):
+                result += word1[i]
+            if i < len(word2):
+                result += word2[i]
+
+        return result
+solution = Solution()
+
+
