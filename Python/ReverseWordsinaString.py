ce02a1d36ddcc551a0bf366dcbb21131d07fb052
arko1995 <shahriarsohelarko@gmail.com>
2026-08-27 10:08:31 +0600

    daily challenge

---
+++ Python/ReverseWordsinaString.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.split()
+        
+        return " ".join(reversed(s))
+    
+
+solution = Solution()
+print(solution.reverseWords("  hello world  "))
