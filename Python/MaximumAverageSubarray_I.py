24edc433db33f9f755df5278b8afd398a7ff6fcf
arko1995 <shahriarsohelarko@gmail.com>
2026-09-09 15:24:29 +0600

    leetcode 75

--- Python/MaximumAverageSubarray_I.py
+++ Python/MaximumAverageSubarray_I.py
@@ -1,16 +1,12 @@
-from functools import reduce
-
-
 class Solution:
     def findMaxAverage(self, nums: list[int], k: int) -> float:
+        window_sum = sum(nums[:k])
+        maxAverage = window_sum
 
-        maxAverage = float("-inf")
-
-        for i in range(0, len(nums) - k + 1):
-            j = i + k
-            total = reduce(lambda x, y: x + y, nums[i:j])
-
-            maxAverage = max(maxAverage, total)
+        for i in range(k, len(nums)):
+            window_sum += nums[i]
+            window_sum -= nums[i - k]
+            maxAverage = max(maxAverage, window_sum)
 
         return maxAverage / k
 
